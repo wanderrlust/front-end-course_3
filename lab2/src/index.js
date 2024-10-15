3fd41117777b519ca0991c4d8e620451173691bc
wanderrlust <kuchmakivan@gmail.com>
2024-10-15 18:44:54 +0300

    Added Users-page

--- lab2/src/index.js
+++ lab2/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Users from "./pages/Users";
 import Layout from "./components/Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,6 +13,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Home />} />
+					<Route path="users" element={<Users />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
---
+++ lab2/src/pages/Users.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+	return (
+		<>
+			<div className="single-page-title">
+				<h2>Welcome to Users-Page</h2>
+			</div>
+		</>
+	);
+}
