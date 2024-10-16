d05ae16b9b742f7756a37750c79442246b52ee7c
wanderrlust <kuchmakivan@gmail.com>
2024-10-16 15:35:47 +0300

    Init lab3 project

---
+++ lab3/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+    return (
+        <h1>Hello world!</h1>
+    )
+}
+root.render(<App />);
