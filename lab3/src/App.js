cf86021665e38917d0cd1c9ff9042505937964ef
wanderrlust <kuchmakivan@gmail.com>
2024-10-19 16:37:11 +0300

    Added Counter component, stylized

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,26 +1,46 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./reset.css";
 import "./Style.css";
 
 const ThemeContext = React.createContext(null);
+const CounterContext = React.createContext(0);
 
 export default function App() {
 	const [theme, setTheme] = useState("light");
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		const handleClick = () => {
+			setCount((count) => count + 1);
+		};
+
+		document.addEventListener("click", handleClick);
+
+		return () => {
+			document.removeEventListener("click", handleClick);
+		};
+	}, [count]);
 
 	return (
-		<ThemeContext.Provider value={theme}>
-			<Form />
-			<label>
-				<input
-					type="checkbox"
-					checked={theme === "dark"}
-					onChange={(e) => {
-						setTheme(e.target.checked ? "dark" : "light");
-					}}
-				/>
-				Змінити на темну тему
-			</label>
-		</ThemeContext.Provider>
+		<CounterContext.Provider value={count}>
+			<ThemeContext.Provider value={theme}>
+				<Form />
+				<label>
+					<input
+						type="checkbox"
+						checked={theme === "dark"}
+						onClick={(e) => {
+							e.stopPropagation();
+						}}
+						onChange={(e) => {
+							setTheme(e.target.checked ? "dark" : "light");
+						}}
+					/>
+					Змінити на темну тему
+				</label>
+				<CounterFixed />
+			</ThemeContext.Provider>
+		</CounterContext.Provider>
 	);
 }
 
@@ -34,6 +54,7 @@ const Form = () => {
 				<Button>Кнопка 1</Button>
 				<Button>Кнопка 2</Button>
 			</Panel>
+			<Counter />
 		</div>
 	);
 };
@@ -54,4 +75,21 @@ const Button = ({ children }) => {
 	const theme = useContext(ThemeContext);
 	const className = "button-" + theme;
 	return <button className={className}>{children}</button>;
+};
+
+const Counter = () => {
+	const count = useContext(CounterContext);
+	const theme = useContext(ThemeContext);
+	const className = "counter-" + theme;
+
+	return (
+		<div className={className}>
+			<h2>Кількість кліків = {count}</h2>
+		</div>
+	);
+};
+
+const CounterFixed = () => {
+	const count = useContext(CounterContext);
+	return <div className="counter-fixed">Кількість кліків = {count}</div>;
 };
