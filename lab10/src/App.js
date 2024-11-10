b9005a967fdca97f91ab02bb241cc35be9700282
wanderrlust <kuchmakivan@gmail.com>
2024-11-10 15:25:28 +0200

    Added React.lazy and Suspense to App.js

--- lab10/src/App.js
+++ lab10/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./reset.css";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import Settings from "./components/Settings";
+
+const Home = lazy(() => import("./components/Home"));
+const Profile = lazy(() => import("./components/Profile"));
+const Settings = lazy(() => import("./components/Settings"));
 
 const App = () => {
 	console.log("APP was rendered");
@@ -26,11 +27,13 @@ const App = () => {
 					</ul>
 				</nav>
 
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/profile" element={<Profile />} />
-					<Route path="/settings" element={<Settings />} />
-				</Routes>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/profile" element={<Profile />} />
+						<Route path="/settings" element={<Settings />} />
+					</Routes>
+				</Suspense>
 			</>
 		</Router>
 	);
