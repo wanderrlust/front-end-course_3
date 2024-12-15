b99a431d86b4d51665eb053585e6140a92a224a1
wanderrlust <kuchmakivan@gmail.com>
2024-12-15 12:31:29 +0200

    added packages: formik&yup

--- lab15/src/App.js
+++ lab15/src/App.js
@@ -1,21 +1,6 @@
-import React from "react";
-import "./reset.css"
-
+import React from 'react';
+import './reset.css';
 
 export default function App() {
-	return (
-		<>
-			<h1 style={{ textAlign: "center", color: "teal" }}>Hello world</h1>
-			<p
-				style={{
-					textAlign: "center",
-					color: "darkgreen",
-					fontSize: "20px",
-					fontWeight: "bold",
-				}}
-			>
-				React App has been launched successfully
-			</p>
-		</>
-	);
+	return <></>;
 }
